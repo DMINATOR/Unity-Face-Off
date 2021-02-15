1e86244d0b9b8a22423d50c7a26a2d890f9235f3
Dmitri Kuznetsov <kna_int@mail.ru>
2021-02-15 21:08:11 +0200

    Added code to delete game object children

--- Assets/Scripts/Generation/SceneGenerator.cs
+++ Assets/Scripts/Generation/SceneGenerator.cs
@@ -37,12 +37,47 @@ public class SceneGenerator : MonoBehaviour
         {
             Debug.Log("Generation started");
 
+            foreach(var setting in Locator.Settings)
+            {
+                GenerateLayer(setting);
+            }
+
             Debug.Log("Generation finished");
         }
         catch(Exception ex)
         {
             Debug.LogError(ex);
             throw new Exception("Generation failed", ex);
+        }
+    }
+
+    public void GenerateLayer(SceneGenerationSettings settings)
+    {
+        try
+        {
+            Debug.Log($"Generating for settings '{settings}'");
+
+            if ( settings.DestinationLayer == null )
+            {
+                throw new Exception("No destination layer is provided!");
+            }
+
+            if (settings.SourceTexture == null)
+            {
+                throw new Exception("No source texture is provided!");
+            }
+
+            // Remove all child objects
+            foreach (Transform child in settings.DestinationLayer.transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+
+        }
+        catch(Exception ex)
+        {
+            Debug.LogError(ex);
+            throw new Exception($"Failed to generate for setting '{settings.Name}'", ex);
         }
     }
 
