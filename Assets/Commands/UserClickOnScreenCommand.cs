cc66e9ad309977707cfa75dac66885bad6962536
Dmitri Kuznetsov <kna_int@mail.ru>
2021-03-05 22:18:16 +0200

    Progress so far, not working

--- Assets/Commands/UserClickOnScreenCommand.cs
+++ Assets/Commands/UserClickOnScreenCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,7 +35,12 @@ public class UserClickOnScreenCommand : ICommand
 
         if( currentTile != null )
         {
-            _tileMap.SetTile(currentPosition, null);
+            var sprite = _tileMap.GetSprite(currentPosition);
+            var renderer = _tileMap.GetComponent<TilemapRenderer>();
+
+            CreateOrUpdateRenderTexture(sprite, renderer);
+
+            _tileMap.RefreshAllTiles();
         }
     }
 
@@ -42,4 +48,130 @@ public class UserClickOnScreenCommand : ICommand
     {
         throw new System.NotImplementedException();
     }
+
+    private void CreateOrUpdateRenderTexture(Sprite sprite, TilemapRenderer renderer)
+    {
+        if (renderer == null || renderer.material == null)
+        {
+            Debug.Log($"No renderer detected!");
+        }
+
+        var isMaskTextureCreated = renderer.material.GetFloat("MaskTextureCreated");
+        var textureToModify = (Texture2D)sprite.texture;
+
+        if (isMaskTextureCreated != 1.0f)
+        {
+            // Modify source texture
+            Debug.Log($"Creating Modified texture...");
+
+            if (textureToModify == null)
+            {
+                throw new Exception("No source texture detected!");
+            }
+            else
+            {
+                // Create new texture
+                var newTextureToModify = new Texture2D(textureToModify.width, textureToModify.height, textureToModify.format, false, false)
+                {
+                    alphaIsTransparency = textureToModify.alphaIsTransparency,
+                    anisoLevel = textureToModify.anisoLevel,
+                    filterMode = textureToModify.filterMode,
+                    hideFlags = textureToModify.hideFlags,
+                    minimumMipmapLevel = textureToModify.minimumMipmapLevel,
+                    mipMapBias = textureToModify.mipMapBias,
+                    requestedMipmapLevel = textureToModify.requestedMipmapLevel,
+                    wrapMode = textureToModify.wrapMode,
+                    wrapModeU = textureToModify.wrapModeU,
+                    wrapModeV = textureToModify.wrapModeV,
+                    wrapModeW = textureToModify.wrapModeW
+                };
+
+                var textureData = textureToModify.GetPixelData<Color>(0);
+                newTextureToModify.SetPixelData(textureData, 0);
+
+                Debug.Log($"... created");
+
+                renderer.material.SetTexture("_MainTex", newTextureToModify);
+                renderer.material.mainTexture = newTextureToModify;
+
+                renderer.material.SetFloat("MaskTextureCreated", 1.0f);
+
+                textureToModify = newTextureToModify;
+            }
+        }
+        else
+        {
+           textureToModify = (Texture2D)renderer.material.GetTexture("_MainTex");
+        }
+
+        // Modify texture here
+        var pointX = sprite.rect.xMin;
+        var pointY = sprite.rect.yMin;
+
+        var previousColor = textureToModify.GetPixel((int)pointX, (int)pointY);
+        var newColor = Color.white;
+
+        Debug.Log($"{pointX},{pointY} [{previousColor}] -> [{newColor}]");
+
+        for(var x = 0; x < sprite.rect.xMax; x++)
+        {
+            for(var y = 0; y < sprite.rect.yMax; y++)
+            {
+                textureToModify.SetPixel(
+                    (int)(pointX + x),
+                    (int)(pointY + y), 
+                    newColor);
+            }
+        }
+
+        textureToModify.Apply();
+    }
+
+    //private Texture2D GetTextureToModify(TilemapRenderer renderer)
+    //{
+    //    var isMaskTextureCreated = renderer.material.GetFloat("MaskTextureCreated"); 
+    //    var textureToModify = (Texture2D)renderer.material.GetTexture("_MainTex");
+
+    //    if (isMaskTextureCreated != 1.0f)
+    //    {
+    //        // Modify source texture
+    //        Debug.Log($"Creating Modified texture...");
+
+    //        if (textureToModify == null)
+    //        {
+    //            throw new Exception("No source texture detected!");
+    //        }
+    //        else
+    //        {
+    //            // Create new texture
+    //            var newTextureToModify = new Texture2D(textureToModify.width, textureToModify.height, textureToModify.format, false, false)
+    //            {
+    //                alphaIsTransparency = textureToModify.alphaIsTransparency,
+    //                anisoLevel = textureToModify.anisoLevel,
+    //                filterMode = textureToModify.filterMode,
+    //                hideFlags = textureToModify.hideFlags,
+    //                minimumMipmapLevel = textureToModify.minimumMipmapLevel,
+    //                mipMapBias = textureToModify.mipMapBias,
+    //                requestedMipmapLevel = textureToModify.requestedMipmapLevel,
+    //                wrapMode = textureToModify.wrapMode,
+    //                wrapModeU = textureToModify.wrapModeU,
+    //                wrapModeV = textureToModify.wrapModeV,
+    //                wrapModeW = textureToModify.wrapModeW
+    //            };
+
+    //            var textureData = textureToModify.GetPixelData<Color>(0);
+    //            newTextureToModify.SetPixelData(textureData, 0);
+
+    //            Debug.Log($"... created");
+
+    //            renderer.material.SetTexture("_MainTex", newTextureToModify);
+
+    //            renderer.material.SetFloat("MaskTextureCreated", 1.0f);
+
+    //            textureToModify = newTextureToModify;
+    //        }
+    //    }
+
+    //    return textureToModify;
+    //}
 }
